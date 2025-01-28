bd2ec417e4fcb77b7ddcb882f49ede2adcc09379
lukegalea2 <50233037+lukegalea2@users.noreply.github.com>
2025-01-28 11:18:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Server is running on port 8080");
 });
